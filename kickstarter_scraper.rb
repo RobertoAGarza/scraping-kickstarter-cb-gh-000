b1e1ebd531f9739f98e7f2ce6e0853935b92566d
Roberto Garza <robertg4567@hotmail.com>
2018-08-08 16:40:45 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,6 +4,8 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogori::HTML(html)
-  binding.pry
+  
 end
+
 create_project_hash
+binding.pry
